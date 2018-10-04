55477d395102041b52d9ee39afde0dfeefa3ce33
Alex Dunn <adunn@johnson-rauhoff.com>
2018-10-04 08:18:56 -0400

    Trying out a 'filled' renderer

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function main() {
   let random = randomArray(20);
   let rows = [];
   quicksort(random, (list) => rows.push(list));
-  render(rows);
+  renderFilled(rows);
 }
 
 
@@ -13,7 +13,7 @@ function main() {
  * Renderer
  */
 
-function render(rows) {
+function renderStroked(rows) {
   let canvas = document.querySelector('canvas');
   canvas.width = window.innerWidth * window.devicePixelRatio;
   canvas.height = window.innerHeight * window.devicePixelRatio;
@@ -28,19 +28,62 @@ function render(rows) {
 
     let v = (1 - row[0] / 100) * ysize;
     ctx.strokeStyle = 'rgba(0, 0, 0, 0.25)';
-    ctx.lineWidth = 2 * window.devicePixelRatio;
     ctx.lineWidth = y / rows.length * 4 + 2;
     ctx.beginPath();
     ctx.moveTo(0 * xsize, y * yspace + v);
     for (let x = 1; x < row.length; x++) {
       let v = (1 - row[x] / 100) * ysize;
-
-
       ctx.lineTo(x * xsize, y * yspace + v);
     }
     ctx.stroke();
   }
 }
+
+
+function renderFilled(rows) {
+  let canvas = document.querySelector('canvas');
+  canvas.width = window.innerWidth * window.devicePixelRatio;
+  canvas.height = window.innerHeight * window.devicePixelRatio;
+  let ctx = canvas.getContext('2d');
+
+
+  let c = 30;
+  ctx.fillStyle = rgba(c, c, c, 1);
+  //ctx.fillStyle = rgba(255, 255, 255, 1);
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  const xsize = canvas.width / (rows[0].length - 1);
+  const ysize = xsize * 5;
+  const yspace = (canvas.height - ysize) / rows.length;
+
+  for (let y = 0; y < rows.length; y++) {
+    let row = rows[y];
+
+    let v = (1 - row[0] / 100) * ysize;
+    let depth = y / rows.length;
+    
+    ctx.beginPath();
+    ctx.moveTo(canvas.width, canvas.height);
+    ctx.lineTo(0, canvas.height);
+    ctx.lineTo(0 * xsize, y * yspace + v);
+
+    for (let x = 1; x < row.length; x++) {
+      let v = (1 - row[x] / 100) * ysize;
+      ctx.lineTo(x * xsize, y * yspace + v);
+    }
+
+    ctx.closePath();
+
+    let color = ((depth) * 0.5 + 0.25) * 255;
+    ctx.fillStyle = rgba(color, color, color, 1);
+    ctx.fill();
+  }
+}
+
+function rgba(r, g, b, a) {
+  return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + a + ')';
+}
+
 
 
 
